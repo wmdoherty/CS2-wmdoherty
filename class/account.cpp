173a6e300bc0414c6922d26efad9c30591bc484f
Will Doherty <wmdoherty@mavs.coloradomesa.edu>
2021-02-01 10:48:45 -0700

    class

--- class/account.cpp
+++ class/account.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class account{
-    
+const int MAXACCOUNT = 1000;
+
+class Account{
+    int amount; // in pennies
+    public;
+    Account(){
+        amount=0;
+    }
+
+    void deposit(int newAmount){
+        amount += newAmount;
+    }
+
+    int withdraw(int amountTaken){
+        if (amount >= amountTaken) {
+            amount -= amountTaken;
+            return amountTaken;
+        }
+        else
+            return 0;
+    }
+
+    int getAmount(){
+        return amount;
+    }
+
+    double getAmountDollars(){
+        return (double)amount/100.;
+    }
+
+    void output() {
+        cout << amount << endl;
+    }
+};
+
+Account accounts[MAXACCOUNT];
+
+int main(){
+    Account a;
+    a.deposit(100000);
+    a.output();
+
+    return 0;
 }
