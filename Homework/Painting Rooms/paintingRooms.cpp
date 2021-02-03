c0399a9a16a0c8100e0405d4df1403508a6c70e6
Will Doherty <wmdoherty@mavs.coloradomesa.edu>
2021-02-03 10:13:52 -0700

    makefile

--- Homework/Painting Rooms/paintingRooms.cpp
+++ Homework/Painting Rooms/paintingRooms.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -19,13 +20,36 @@ int main(){
     cout << "How many rooms are you going to paint?" << endl;
     cin >> rooms;
 
-    int walls=0;
-    for (int i=0; 1 < rooms; i++) {
-        int newWalls;
+    double sqrft = 0;
+
+    for (int i=0; i < rooms; i++) {
+        int walls;
         cout << "Walls in room " << i << ":";
-        cin >> newWalls;
-        walls += newWalls;
+        cin >> walls;
+        for(int j=0; j < walls; j++) {
+            double height;
+            double width;
+            cout << "Height and width of wall " << j << ":";
+            cin >> height >> width;
+            sqrft += (height * width);
+            int windows;
+            cout << "How many windows or doors are on the wall?" << endl;
+            cin >> windows;
+            if (windows != 0){
+                for (int z=0; z < windows; z++){
+                    double wHeight;
+                    double wWidth;
+                    cout << "Height and width of window/door " << z << ":";
+                    cin >> wHeight >> wWidth;
+                    sqrft -= (wHeight * wWidth);
+                }
+            }
+        }
     }
+
+    double gallonsNeeded = ceil(sqrft/coverage);
+    cout << "You will need " << gallonsNeeded << " which will cost $" <<  gallonsNeeded * paintCost <<"." << endl;
+
 
     return 0;
 }
