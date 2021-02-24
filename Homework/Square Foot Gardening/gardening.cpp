c321fc1a8562c7cd6ac09e23d734ab4bcacde67a
Will Doherty <wmdoherty@mavs.coloradomesa.edu>
2021-02-24 10:58:28 -0700

    derived classes

--- Homework/Square Foot Gardening/gardening.cpp
+++ Homework/Square Foot Gardening/gardening.cpp
@@ -22,6 +22,48 @@ class Box{
 
 };
 
+class Plant {
+    string name;
+    vector <int> dimensions;
+
+    public:
+    Plant(string newName) {
+        name=newName;
+    }
+    void width(int newDimension) {
+        dimensions.push_back(newDimension);
+        dimensions.push_back(newDimension);
+    }
+};
+
+class SmallPlant: public Plant {
+    public:
+    SmallPlant(string newName="Small Plant"):Plant(newName){
+        width(1);
+    }
+};
+
+class MediumPlant: public Plant {
+    public:
+    MediumPlant(string newName="Medium Plant"):Plant(newName){
+        width(2);
+    }
+};
+
+class Shrub: public Plant {
+    public:
+    Shrub(string newName="Shrub"):Plant(newName){
+        width(3);
+    }
+};
+
+class Tree: public Plant {
+    public:
+    Tree(string newName="Tree"):Plant(newName){
+        width(4);
+    }
+};
+
 int main(){
     Box garden;
     cout << garden.getBoxDimensions() << endl;
