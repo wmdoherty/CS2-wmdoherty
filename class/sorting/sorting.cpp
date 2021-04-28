163b0bded45d6e341adfced80ca487653c5a7366
Will Doherty <wmdoherty@mavs.coloradomesa.edu>
2021-04-28 10:51:54 -0600

    sorting

--- class/sorting/sorting.cpp
+++ class/sorting/sorting.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,27 +10,41 @@ bool find(vector <int> ints, int seekValue){
     int high=ints.size()-1;
     if (ints[low]==seekValue) return true;
     if (ints[high]==seekValue) return true;
-    while(true){
+    while((low+1)!=high){
         int m=(low+high)/2;
         if (ints[m]==seekValue) return true;
         if (ints[m]<seekValue) low=m;
         if (ints[m]>seekValue) high=m;
-        if(low==high) return false;
     }
+    return (ints[low]==seekValue) || (ints[high]=seekValue);
 }
 
 int main(){
     vector <int> ints;
-    ints.push_back(1);
-    ints.push_back(2);
-    ints.push_back(10);
-    ints.push_back(16);
-    ints.push_back(100);
-    ints.push_back(1000);
-    ints.push_back(10000);
+
+    srand(time(NULL));
+    for(int i=0;i<100;i++)
+        ints.push_back(rand()&1000);
+
+    sort(ints.begin(), ints.end());
+
     for (unsigned i=0; i<ints.size(); i++){
         if(i>0) cout << ",";
         cout << ints[i];
+    }
+    cout << endl;
+
+    if(find(ints,17)){
+        cout << "Ints contains 17" << endl;
+    }
+    else{
+        cout << "Could not find value" << endl;
+    }
+    if(find(ints,100)){
+        cout << "Ints contains 100" << endl;
+    }
+    else{
+        cout << "Could not find value" << endl;
     }
     cout << endl;
     return 0;
