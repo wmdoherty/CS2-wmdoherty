4d506f622f5b76ae11d33cdecc2964106dff289f
Will Doherty <wmdoherty@mavs.coloradomesa.edu>
2021-04-28 20:14:32 -0600

    getVal

--- Homework/spreadsheat/Spreadsheet.cpp
+++ Homework/spreadsheat/Spreadsheet.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <string>
 
@@ -29,6 +30,14 @@ class Spreadsheet{
 		cell.push_back(entry);
 		cells.push_back(cell);
 	}
+
+	void readCell(string cellName){
+		for(unsigned i=0; i<cells.size(); i++){
+			for(unsigned j=0; j<cells[i][1].size(); j++){
+				
+			}
+		}
+	}
 	
 	void output(){
 		for(unsigned i=0; i<cells.size(); i++){
@@ -39,24 +48,33 @@ class Spreadsheet{
 		}
 	}
 
-	string getCellValue(string name){
+	string getCellValue(string cellName){
 		for(unsigned i=0; i<cells.size(); i++){
-			if (cells[i][0]==name)
+			if (cells[i][0]==cellName)
 				return cells[i][1];
 		}
 		return "Not a valid cell name";
 	}
 };
 
+bool isNumber(const string& str){
+    for(char const &c : str){
+        if (isdigit(c)==0) return false;
+    }
+    return true;
+}
+
 int main() {
 	Spreadsheet a;
 	a.addCellNumber("3");
 	a.addCellNumber("2");
-	a.addCellNumber("7");
+	a.addCellNumber("7.2");
 	a.addCellLetter("32");
 	a.addCellNumber("22");
 	a.output();
 	cout << endl;
 	cout << a.getCellValue("A3") << endl;
 	cout << a.getCellValue("A4") << endl;
+	cout << stod(a.getCellValue("A3"))+stod(a.getCellValue("A1")) << endl;
+	cout << a.getCellValue("B1")[1] << endl;
 }
