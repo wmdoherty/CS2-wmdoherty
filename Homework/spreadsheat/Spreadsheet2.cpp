0de245ff687614dee2433f63fb2dfa62921ffa3d
Will Doherty <wmdoherty@mavs.coloradomesa.edu>
2021-05-20 21:01:32 -0600

    working?

--- Homework/spreadsheat/Spreadsheet2.cpp
+++ Homework/spreadsheat/Spreadsheet2.cpp
@@ -4,22 +4,9 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
-
-class Cell{
-	int cellNumber;
-	char cellLetter;
-	string formula;
-	double value;
-	public:
-	Cell(){
-		cellNumber=0;
-		cellLetter='A';
-		formula="";
-		value=0;
-	}
-};
 
 class StackException{
     protected:
@@ -40,7 +27,7 @@ template <class t> class LLNode{
 
 };
 
-template <class t> class Stack{ //stack of integers -> template
+template <class t> class Stack{ 
     LLNode<t> *head;
     public:
     Stack(){
@@ -61,7 +48,7 @@ template <class t> class Stack{ //stack of integers -> template
         p->next=head;
         head=p;
     }
-    int pop(){ //modifies stack by removing top item
+    double pop(){ //modifies stack by removing top item
         t retval;
         retval=top();
         LLNode<t> *p;
@@ -70,7 +57,7 @@ template <class t> class Stack{ //stack of integers -> template
         delete p;
         return retval;
     }
-    int top() const{ //looks at top item
+    double top() const{ //looks at top item
         if(isEmpty()) throw StackException("Stack Underflow");
         return head->value;
     }
@@ -94,41 +81,120 @@ class MathException: public StackException{
     }
 };
 
-double calculate(string expression){
-    stringstream in(expression);
-    Stack<double> s; // 100 elements
-    string input="";
-    while (in>>input){
-        if (input=="/"){
-            double a=s.pop();
-            double b=s.pop();
-            if (b==0.0) throw MathException("Divide by zero");
-            s.push(a/b);
+class Spreadsheet{
+	vector <string> cell;
+	int cellNumber;
+	char cellLetter;
+    string value;
+	vector <vector<string> > cells;
+	public:
+	Spreadsheet(){
+		cellNumber=0;
+		cellLetter='A';
+        value ="0";
+	}
+	void addCellNumber(string entry){
+		cellNumber++;
+		cell.clear();
+		cell.push_back(cellLetter+to_string(cellNumber));
+		cell.push_back(entry);
+		cells.push_back(cell);
+	}
+	void addCellLetter(string entry){
+		cell.clear();
+		cellLetter++;
+		cellNumber=1;
+		cell.push_back(cellLetter+to_string(cellNumber));
+		cell.push_back(entry);
+		cells.push_back(cell);
+	}
+
+    string getCellEntry(string cellName){
+        for(unsigned i=0; i<cells.size(); i++){
+			if(cells[i][0]==cellName)
+                return cells[i][1];
+		}
+        return "Invalid cell";
+    }
+    void addCellValue(string cellName, string value){
+        for(unsigned i=0; i<cells.size(); i++){
+			if(cells[i][0]==cellName)
+                cells[i].push_back(value);
+		}
+    }
+
+    string getCellValue(string cellName){
+        for(unsigned i=0; i<cells.size(); i++){
+			if(cells[i][0]==cellName)
+                return cells[i][2];
+		}
+        return "Invalid cell";
+    }
+
+    bool isCell(string str){
+        string cellName="";
+        for (char letter='A'; letter<='Z'; letter++){
+            for(int i=1; i<100; i++){
+                cellName=letter+to_string(i);
+                if (cellName==str) return true;
+            }
         }
-        else if(input=="-"){
-            double a=s.pop();
-            double b=s.pop();
-            s.push(a-b);
-        }
-        else if (input=="+"){
-            double a=s.pop();
-            double b=s.pop();
-            s.push(a+b);
-        }
-        if (input=="*"){
-            double a=s.pop();
-            double b=s.pop();
-            s.push(a*b);
-        }
-        else if (isNumber(input)){
-            s.push(atoi(input.c_str()));
-        }
-    } 
-    return s.top();
-}
+        return cellName==str;
+    }
+
+    double calculate(string expression){
+        stringstream in(expression);
+        Stack<double> s; // 100 elements
+        string input="";
+        while (in>>input){
+            if(isCell(input)){
+                s.push(stod(getCellValue(input)));
+            }
+            if (input=="/"){
+                double a=s.pop();
+                double b=s.pop();
+                if (b==0) throw MathException("Divide by zero");
+                s.push(b/a);
+            }
+            else if(input=="-"){
+                double a=s.pop();
+                double b=s.pop();
+                s.push(b-a);
+            }
+            else if (input=="+"){
+                double a=s.pop();
+                double b=s.pop();
+                s.push(a+b);
+            }
+            else if (input=="*"){
+                double a=s.pop();
+                double b=s.pop();
+                s.push(a*b);
+            }
+            else if (isNumber(input)){
+                s.push(stod(input.c_str()));
+            }
+        } 
+        return s.top();
+    }
+};
 
 int main() {
-    cout << calculate("7 654 +") << endl;
-    cout << calculate("6 0 /");
+    Spreadsheet a;
+    string line;
+    char letter ='A';
+    int number=1;
+    ifstream file("text.txt");
+    if (file.is_open()){
+        while ( getline (file,line) ){
+            string cell=letter+to_string(number);
+            a.addCellNumber(line);
+            double value = a.calculate(a.getCellEntry(cell));
+            a.addCellValue(cell, to_string(value));
+            cout << cell << " = " << a.getCellValue(cell) << endl;
+            number++;
+        }
+    file.close();
+    }
     return 0;
 }
